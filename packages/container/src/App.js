3e17c4a21d04a5a47dfbcd3e390317e117f00491
Lukman Hidayah <lukman.hidayah@ottodigital.id>
2022-05-14 22:35:00 +0700

    Add prefix classname

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import MarketingApp from "../components/MarketingApp";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Header from "../components/Header";
 import { BrowserRouter } from "react-router-dom";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
