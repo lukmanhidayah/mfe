c936ad7ac4006c4fc9b7d5652b1129d4755c2a05
Lukman Hidayah <lukman.hidayah@ottodigital.id>
2022-05-14 22:00:30 +0700

    Config Deploy marketing

--- packages/marketing/components/Pricing.js
+++ packages/marketing/components/Pricing.js
@@ -17,6 +17,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
+      
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
       </Link>{' '}
