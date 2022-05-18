2f9db1bccd2133941f955fc368ed2b40f759e199
Lukman Hidayah <lukman.hidayah@ottodigital.id>
2022-05-18 19:55:31 +0700

    Update auth

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,19 +4,19 @@ const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
-console.log(domain)
 
 const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/container/latest/"
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import MarketingApp from "../components/MarketingApp";
-import { StylesProvider, createGenerateClassName } from "@material-ui/core";
-import Header from "../components/Header";
-import { BrowserRouter } from "react-router-dom";
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import Progress from './components/Progress';
+import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: "co",
+  productionPrefix: 'co',
 });
 
 export default () => {
@@ -14,7 +21,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
